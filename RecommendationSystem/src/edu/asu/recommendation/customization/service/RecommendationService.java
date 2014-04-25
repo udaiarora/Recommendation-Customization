c1fa05cd4f2746bed1975bc2500395c46524a6fe
dmanjuna <dmanjuna@asu.edu>
2014-04-24 20:37:52 -0600

    Modified Recommendation Controller and Interface

--- RecommendationSystem/src/edu/asu/recommendation/customization/service/RecommendationService.java
+++ RecommendationSystem/src/edu/asu/recommendation/customization/service/RecommendationService.java
@@ -3,4 +3,5 @@ package edu.asu.recommendation.customization.service;
 public interface RecommendationService 
 {
 
+	public boolean getTemplates(Integer[] templates);
 }
--- RecommendationSystem/src/edu/asu/recommendation/customization/service/impl/RecommendationImpl.java
+++ RecommendationSystem/src/edu/asu/recommendation/customization/service/impl/RecommendationImpl.java
@@ -1,7 +1,14 @@
 package edu.asu.recommendation.customization.service.impl;
 
 import edu.asu.recommendation.customization.service.RecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import edu.asu.recommendation.customization.dao.*;
+import edu.asu.recommendation.customization.dao.impl.UserDAOImpl;
+import edu.asu.recommendation.customization.service.UserService;
+
+@Service
 public class RecommendationImpl implements RecommendationService
 {
 
